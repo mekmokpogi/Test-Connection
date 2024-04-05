26297dbc88206e2b8028baede25e18678b891cf1
MICHAEL FORTON <MICHAEL FORTON@DESKTOP-QJH73MG>
2024-04-05 14:49:35 +0800

    Revert "long white space"
    This reverts commit 7d9aff525a39ba3e7d32849431b2c7932df1750f.

--- TestConnectionToGit/TestConnectionToGit/Form1.vb
+++ TestConnectionToGit/TestConnectionToGit/Form1.vb
@@ -11,14 +11,6 @@ Public Class Form1
                 MessageBox.Show("Not Connected")
             End Try
 
-
-
-
-
-
-
-
-
         End Using
     End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
