d62c9c080616da49c080123f33493607146c8e9a
Nyxpotato <139918809+Nyxpotato@users.noreply.github.com>
2024-04-05 15:12:11 +0800

    Revert "red push"
    This reverts commit 08fc3848ec2cd132cb90d3c89b3ce96e0874c57e.

--- TestConnectionToGit/TestConnectionToGit/Form1.vb
+++ TestConnectionToGit/TestConnectionToGit/Form1.vb
@@ -19,6 +19,6 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ConnectToDatabase()
 
-        'red'
+        'kument'
     End Sub
 End Class
